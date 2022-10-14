7ff9b78aa2213201a5a646ebf5b1f5c2a8dad925
Klaus Nascimento <klausnascimento@gmail.com>
2022-10-14 14:49:03 -0300

    Listar Registros

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
+
+Route::get('post', [PostController::class, 'index']);
+
+// $this->get('url', 'NomeController@index')->name('url.index');
+// $this->get('url/create', 'NomeController@create')->name('url.create');
+// $this->post('url', 'NomeController@store')->name('url.store');
+// $this->get('url/{id}', 'NomeController@show')->name('url.show');
+// $this->get('url/{id}/edit', 'NomeController@edit')->name('url.edit');
+// $this->put('url/{id}', 'NomeController@update')->name('url.update');
+// $this->delete('url/{id}', 'NomeController@destroy')->name('url.destroy');
 
 Route::get('/', function () {
     return view('welcome');
